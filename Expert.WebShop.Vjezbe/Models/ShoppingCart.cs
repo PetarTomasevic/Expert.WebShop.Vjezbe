37a5182d074fe760637a36fbadbb4c68c2aa80e8
Petar <petar.tomasevic@magnanimous.info>
2023-04-22 10:59:22 +0200

    zadnje

---
+++ Expert.WebShop.Vjezbe/Models/ShoppingCart.cs
@@ -0,0 +1,11 @@
+﻿namespace Expert.WebShop.Vjezbe.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string OrderedByName { get; set; }
+        public string OrderedByEmail { get; set; }
+        public string OrderId { get; set; }
+    }
+}
