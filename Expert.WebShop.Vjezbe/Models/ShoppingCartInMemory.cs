ed656844c0b3e82f2d28f8e1a9d6f9b122542150
Petar <petar.tomasevic@magnanimous.info>
2023-04-22 11:29:17 +0200

    commit

---
+++ Expert.WebShop.Vjezbe/Models/ShoppingCartInMemory.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Expert.WebShop.Vjezbe.Models
+{
+    public class ShoppingCartInMemory
+    {
+        private readonly HttpClient _httpClient;
+        private List<ShoppingCart> SelectedItems { get; set; } = new();
+
+        //konstruktor klase
+        public ShoppingCartInMemory(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
