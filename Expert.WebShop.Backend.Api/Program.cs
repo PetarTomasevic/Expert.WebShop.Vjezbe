91e35bcd1704671427d476d6fadd5c14ec1f2fac
Petar <petar.tomasevic@magnanimous.info>
2023-05-20 13:24:37 +0200

    zadnje

--- Expert.WebShop.Backend.Api/Program.cs
+++ Expert.WebShop.Backend.Api/Program.cs
@@ -1,5 +1,7 @@
+using Azure.Core;
 using Expert.WebShop.Backend.Api.MojaBaza;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,9 @@ builder.Services.AddDbContext<WebShopVjezba2Context>
     (options => options.
     UseSqlServer("Data Source=DESKTOP-2GP0R2M;Initial Catalog=WebShopVjezba2;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"));
 builder.Services.AddControllers();
+
+builder.Services.AddDirectoryBrowser();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,6 +25,25 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(x => x
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .SetIsOriginAllowed(origin => true) // allow any origin
+               .AllowCredentials()); // allow credentials
+
+app.UseStaticFiles();
+
+app.UseFileServer(
+    new FileServerOptions
+    {
+        FileProvider = new
+        PhysicalFileProvider(
+            Path.Combine(builder.Environment
+            .ContentRootPath, "WebShopPictures")),
+        RequestPath = "/WebShopPictures",
+        EnableDirectoryBrowsing = true
+    }
+    );
 
 app.UseHttpsRedirection();
 
